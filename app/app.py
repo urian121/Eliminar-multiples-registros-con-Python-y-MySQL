6a5ce7eab191b34e1292533a5cc005b3de8b977a
urian <urian1213viera@gmail.com>
2022-11-14 15:21:33 -0500

    Proyecto terminado y funcional

--- app/app.py
+++ app/app.py
@@ -16,19 +16,18 @@ app.secret_key = '32423gfdgdfgdf'
 #Creando mi Decorador para el Home
 @app.route('/', methods=['GET','POST'])
 def inicio():
-        
-        conexion_MySQLdb = connectionBD() #creando mi instancia a la conexion de BD
-        cur      = conexion_MySQLdb.cursor(dictionary=True)
-        #Importante uso f para
-        querySQL = "SELECT * FROM countries LIMIT 50"
-        cur.execute(querySQL) 
-        resultadoBusqueda = cur.fetchall() #fetchall () Obtener todos los registros
-        totalBusqueda = len(resultadoBusqueda) #Total de busqueda
-        
-        cur.close() #Cerrando conexion SQL
-        conexion_MySQLdb.close() #cerrando conexion de la BD
-        return render_template('public/index.html', miData = resultadoBusqueda, total = totalBusqueda)
-      
+    conexion_MySQLdb = connectionBD() #creando mi instancia a la conexion de BD
+    cur      = conexion_MySQLdb.cursor(dictionary=True)
+    #Importante uso f para
+    querySQL = "SELECT * FROM countries LIMIT 50"
+    cur.execute(querySQL) 
+    resultadoBusqueda = cur.fetchall() #fetchall () Obtener todos los registros
+    totalBusqueda = len(resultadoBusqueda) #Total de busqueda
+    
+    cur.close() #Cerrando conexion SQL
+    conexion_MySQLdb.close() #cerrando conexion de la BD
+    return render_template('public/index.html', miData = resultadoBusqueda, total = totalBusqueda)
+    
 
 
 @app.route('/eliminar-pais', methods=['POST'])
@@ -36,14 +35,13 @@ def eliminarSeleccionPais():
     if request.method == 'POST':  	
         idSeleccionados = request.json['idCheckbox']
         print(idSeleccionados) #Ejemplo: 41,42
-        
         '''
         #Ejemplo de
         desarrollador = "urian viera desarrollador"
         urian         = desarrollador.split() #split terminar convirtiendo un string en una lista
         print(urian) #['urian', 'viera', 'desarrollador'] 
         '''
-        #Comprobar si existe un , en la lista
+        #Comprobar si existe una (,) en la lista es que hay mas de un elemento en la lista
         if ',' in idSeleccionados:		
             idSeleccionados = idSeleccionados.split(',')
             #print(idSeleccionados) # ['45', '46']
@@ -59,7 +57,19 @@ def eliminarSeleccionPais():
                 
                 cur.close() #Cerrando conexion SQL
                 conexion_MySQLdb.close() #cerrando conexion de la BD
-    return jsonify({'resp': respuesta, 'ids':idSeleccionados })  
+                return jsonify({'resp': respuesta, 'ids':idSeleccionados,  'totalIds': 'x' }) 
+        else:
+            conexion_MySQLdb = connectionBD() #Hago instancia a mi conexion desde la funcion
+            cur              = conexion_MySQLdb.cursor(dictionary=True)
+            cur.execute('DELETE FROM countries WHERE ID=%s', (idSeleccionados,))
+            conexion_MySQLdb.commit()
+            respuesta = cur.rowcount
+            #print("Registros Borrados = ", respuesta)
+            
+            cur.close() #Cerrando conexion SQL
+            conexion_MySQLdb.close() #cerrando conexion de la BD
+            
+            return jsonify({'resp': respuesta, 'ids':idSeleccionados, 'totalIds': 1})  
 
 
 
